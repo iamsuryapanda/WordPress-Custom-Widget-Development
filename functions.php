6551581276c5c0326c0fed914fd98936e24ca3f7
suryapanda4 <55087897+suryapanda4@users.noreply.github.com>
2019-09-10 13:25:09 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,2 +1,7 @@
 $sidebar_widget = new Custom_theme_widget();
 add_action('widgets_init', array($sidebar_widget, 'reg_sidebar_widget'));
+
+function enqueue_js(){
+  wp_enqueue_script('widget-js',get_template_directory_uri().'widget.js', array('jquery'),'1.0', true);
+}
+add_action('wp_enqueue_scripts', 'enqueue_js');
